c7e44a8195ab1e7b46b79df2f9c39f2ae1a46440
Bradley Rosenfeld <bradley@rosenfeld.xyz>
2015-12-08 12:30:49 -0500

    Changed size of triangles

--- _js/main.js
+++ _js/main.js
@@ -13,7 +13,7 @@
 	var pattern = Trianglify({
 		width: parent.offsetWidth,
 		height: parent.offsetHeight * 1.5,
-		cell_size: 105,
+		cell_size: 80,
 		//seed: document.title, 
 		//x_colors: ['#FFFFFF', '#16577d'],
 	});
